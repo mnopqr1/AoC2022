e08a3bae201067dfdd954dc851c968cac34c11a9
Sam <78932387+mnopqr1@users.noreply.github.com>
2022-12-04 18:14:14 +0100

    fun with function composition

--- day4/bin/main.ml
+++ day4/bin/main.ml
@@ -1,8 +1,11 @@
 module Day4 = struct
 open Aocutils
 
+let (>>) f g = fun x -> g (f x)
+
 (** [parse_line] converts a line of the form "a1-b1,a2-b2" to a pair of pairs of ints *)
-let parse_line l = mappair (fun x -> mappair int_of_string (split2 '-' x)) (split2 ',' l)
+let parse_line = (split2 ',') >> mappair ((split2 '-') >> mappair int_of_string)
+
 let parse = List.map parse_line
 
 let contains (c,d) (a,b) = a >= c && b <= d
@@ -11,7 +14,7 @@ let disjoint (a,b) (c,d) = b < c || d < a
 let cond2 ((a,b),(c,d)) = not (disjoint (a,b) (c,d))
 let solve cond l = List.length (List.filter cond l)
 
-let () = let lines = parse @@ read in
+let () = let lines = parse read in
   print_int (solve cond1 lines); print_newline ();
   print_int (solve cond2 lines); print_newline ();
 
