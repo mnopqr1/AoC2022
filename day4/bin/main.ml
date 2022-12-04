a5df0699fc9d8f4c4e65428cc2651b40c34a3227
Sam <78932387+mnopqr1@users.noreply.github.com>
2022-12-04 11:15:39 +0100

    cleaning up day4

--- day4/bin/main.ml
+++ day4/bin/main.ml
@@ -1,13 +1,12 @@
 module Day4 = struct
 open Aocutils
 
-
 (** [parse_line] converts a line of the form "a1-b1,a2-b2" to a pair of pairs of ints *)
 let parse_line l = mappair (fun x -> mappair int_of_string (split2 '-' x)) (split2 ',' l)
 let parse = List.map parse_line
 
-let contains (a,b) (c,d) = a >= c && b <= d
-let cond1 ((a,b),(c,d)) = contains (a,b) (c,d) || contains (c,d) (a,b)
+let contains (c,d) (a,b) = a >= c && b <= d
+let cond1 ((a1,b1),(a2,b2)) = contains (a1,b1) (a2,b2) || contains (a2,b2) (a1,b1)
 let disjoint (a,b) (c,d) = b < c || d < a
 let cond2 ((a,b),(c,d)) = not (disjoint (a,b) (c,d))
 let solve cond l = List.length (List.filter cond l)
