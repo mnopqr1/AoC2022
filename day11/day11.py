c8d9c9a43615ad6801631d864ad9f5840f74cd0e
Sam <78932387+mnopqr1@users.noreply.github.com>
2022-12-11 08:05:07 +0100

    day 11 part 1

---
+++ day11/day11.py
@@ -0,0 +1,83 @@
+from dataclasses import dataclass
+from typing import List, Callable
+
+debug = False
+VERB = {"+": "increased", "*": "multiplied"}
+@dataclass
+class Item:
+    value: int
+
+    def do(self, op: str, m: str):
+        x = self.value if m == "old" else int(m)
+        if op == "+": self.value += x
+        else: self.value *= x
+        if debug: print(f"   Worry level is {VERB[op]} by {x} to {self.value}")
+        self.value = self.value // 3
+        if debug: print(f"   Monkey gets bored with item. Worry level is divided by 3 to {self.value}")
+
+@dataclass
+class Monkey:
+    id: int
+    items: List[Item]
+    operation: str
+    operand: str
+    divisor: int
+    iftrue: int
+    iffalse: int
+    count: int
+
+    def turn(self, monkeys):
+        if debug: print(f"Monkey {self.id}:")
+        while len(self.items) > 0:
+            self.count += 1
+            curr: Item = self.items.pop(0)
+            if debug: print(f"  Monkey inspects an item with a worry level of {curr.value}.")
+            curr.do(self.operation, self.operand)
+            if curr.value % self.divisor == 0:
+                if debug: print(f"    Current worry level is divisible by {self.divisor}.")
+                dest = self.iftrue
+            else:
+                if debug: print(f"    Current worry level is not divisible by {self.divisor}.")
+                dest = self.iffalse
+            if debug: print(f"    Item with worry level {curr.value} is thrown to monkey {dest}.")
+            monkeys[dest].items.append(curr)
+
+    def __repr__(self):
+        return f"Monkey {self.id}: " + ",".join(str(i.value) for i in self.items) + f"\nOperation: old = old {self.operation} {self.operand}\nDivisor: {self.divisor}\nIf true: {self.iftrue}\nIf false:{self.iffalse}"
+
+
+def parse(block):
+    rel = [b[b.index(":")+1:] for b in block]
+    id = int(block[0].split(" ")[1][:-1])
+    its = [Item(int(x)) for x in rel[1].split(",")]
+    op, mult = rel[2].split(" ")[4:6]
+    div = int(rel[3].split(" ")[-1])
+    ift = int(rel[4].split(" ")[-1])
+    iff = int(rel[5].split(" ")[-1])
+    # print(id, its, op, mult, div, ift, iff)
+    return Monkey(id, its, op, mult, div, ift, iff, 0)
+
+
+def read(filename):
+    with open(filename) as f:
+        lines = [l.rstrip().lstrip() for l in f.readlines()]
+    n = (len(lines) + 1) // 7
+    monkeys: List[Monkey] = []
+    for i in range(n):
+        monkeys.append(parse(lines[7*i:7*i+6]))
+    return monkeys
+
+def round(monkeys):
+    for i in range(len(monkeys)):
+        monkeys[i].turn(monkeys)
+
+if __name__ == "__main__":
+    monkeys = read("input.txt")
+    for i in range(20):
+        if debug: print(f"== Round {i+1} ==")
+        round(monkeys)
+        if debug:
+            for m in monkeys:
+                print(m)
+    cts = sorted(m.count for m in monkeys)
+    print(cts[-1]*cts[-2])
